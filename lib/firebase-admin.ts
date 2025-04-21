6cee3fad3cc3b8cccf1a94132a9a1148a12a159a
amurorei499 <amurorei499@gmail.com>
2025-04-22 00:51:52 +0900

    ver0.3.8

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -3,11 +3,7 @@ import { getAuth } from "firebase-admin/auth";
 import { getFirestore } from "firebase-admin/firestore";
 
 const firebaseAdminConfig = {
-  credential: cert({
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-  }),
+  credential: cert(JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY || '{}')),
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseAdminConfig) : getApps()[0];
